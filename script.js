0ef6d41388e619319640ec5fb88c8e8f50c0ac6e
Aliaksandr Kryskevich <wordpresserinblog@gmail.com>
2019-05-02 20:44:46 +0300

    Script with cycle

--- script.js
+++ script.js
@@ -8,11 +8,61 @@ let appData = {
         income: [],
         savings: false
     };
-let expensesAnswer = prompt("Введите обязательную статью расходов в этом месяце", ""),
-    expensesSum = +prompt("Во сколько обойдется?", "");
+let questionCounter = 0;
 
-appData.expenses[expensesAnswer] = expensesSum;
+for (let i = 0; i < 2; i++) {
+    let expensesAnswer = prompt("Введите обязательную статью расходов в этом месяце", ""),
+        expensesSum = +prompt("Во сколько обойдется?", "");
 
-alert("Ваш бюджет на один день равен " + (appData.budget - appData.expenses[expensesAnswer]) / 30);
+    if (typeof(expensesAnswer) === 'string' && typeof(expensesAnswer) != null && typeof(expensesSum) != null
+        && expensesAnswer != '' && expensesSum != '' && expensesAnswer.length < 50) {
+        appData.expenses[expensesAnswer] = expensesSum;
+    } else {
+        i--;
+    }
+}
+
+// while (questionCounter < 2) {
+//     let expensesAnswer = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         expensesSum = +prompt("Во сколько обойдется?", "");
+
+//     if (typeof(expensesAnswer) === 'string' && typeof(expensesAnswer) != null && typeof(expensesSum) != null
+//         && expensesAnswer != '' && expensesSum != '' && expensesAnswer.length < 50) {
+//         appData.expenses[expensesAnswer] = expensesSum;
+//     } else {
+//         questionCounter--;
+//     }
+
+//     questionCounter++;
+// }
+
+// do {
+//     let expensesAnswer = prompt("Введите обязательную статью расходов в этом месяце", ""),
+//         expensesSum = +prompt("Во сколько обойдется?", "");
+
+//     if (typeof(expensesAnswer) === 'string' && typeof(expensesAnswer) != null && typeof(expensesSum) != null
+//         && expensesAnswer != '' && expensesSum != '' && expensesAnswer.length < 50) {
+//         appData.expenses[expensesAnswer] = expensesSum;
+//     } else {
+//         questionCounter--;
+//     }
+
+//     questionCounter++;
+// } while (questionCounter < 2);
+
+appData.moneyPerDay = appData.budget / 30;
+
+
+alert("Ваш бюджет на один день равен " + appData.moneyPerDay);
+
+if (appData.moneyPerDay < 100) {
+    console.log('Минимальный уровень достатка');
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log('Средний уровень достатка');
+} else if (appData.moneyPerDay > 2000) {
+    console.log('Высокий уровень достатка');
+} else {
+    console.log('Произошла ошибка');
+}
 
 console.log(appData);
